f71662fe75e9cb85b856da5635640eb74a2b2933
Prathamesh Dhande <prathamesh.d@systenics.com>
2025-02-14 17:30:15 +0530

    Ollama Setup

---
+++ Product Classification/ProductClassification.AppHost/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using Aspire.Hosting;
+
+namespace ProductClassification.AppHost
+{
+    public static class Extensions
+    {
+        public static IResourceBuilder<ProjectResource> AddOllamaEndpointToEnvironmentVariables(this IResourceBuilder<ProjectResource> builder, EndpointReference endpoint)
+        {
+            builder.WithEnvironment("OllamaDeepSeek:Url", endpoint);
+            builder.WithEnvironment("OllamaPhi3_8b:Url", endpoint);
+            builder.WithEnvironment("OllamaQwen1_8b", endpoint);
+            return builder;
+        }
+
+        public static IResourceBuilder<PostgresServerResource> AddCommandForClearEvaluationData(this IResourceBuilder<PostgresServerResource> builder)
+        {
+            builder.WithCommand(
+                name: "clearevaldata",
+                displayName: "Clear Evaluation Data",
+                executeCommand: context => ClearEvaluationDataFromDatabase(context, builder),
+                displayDescription: "Clears Evaluation Related Data",
+                confirmationMessage: "Are You Sure You want To Delete Data",
+                iconName: "Delete",
+                iconVariant: IconVariant.Filled
+
+                );
+            return builder;
+        }
+
+        private static async Task<ExecuteCommandResult> ClearEvaluationDataFromDatabase(ExecuteCommandContext context, IResourceBuilder<PostgresServerResource> builder)
+        {
+            string connectionstring = await builder.Resource.GetConnectionStringAsync();
+
+
+            
+            return CommandResults.Success();
+        }
+    }
+}
--- Product Classification/ProductClassification.AppHost/Program.cs
+++ Product Classification/ProductClassification.AppHost/Program.cs
@@ -1,3 +1,6 @@
+using Aspire.Hosting;
+using ProductClassification.AppHost;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 
@@ -7,14 +10,27 @@ var postgresUsernameParameterResource = builder.AddParameter("postgres-user", se
 var postgres = builder.AddPostgres("postgres", postgresUsernameParameterResource, postgresPasswordParameterResource)
                       .WithPgAdmin(containerName: "postgresadmin")
                       .WithLifetime(ContainerLifetime.Persistent)
-                      .WithDataVolume("pgvolume");
+                      .WithDataVolume("pgvolume")
+                      .AddCommandForClearEvaluationData();
 
 var promptevaluationdb = postgres.AddDatabase("promptevaldb", "promptevaluationdb");
 
+var ollama = builder.AddContainer("ollama", "ollama/ollama")
+                    .WithContainerName("ollama-aspire")
+                    .WithHttpEndpoint(11434, 11434, "ollamaendpoint")
+                    .WithLifetime(ContainerLifetime.Persistent)
+                    .WithVolume("ollmodels", "/root/.ollama")
+                    .WithBindMount("./ollamasetup.sh", "/ollamasetup.sh")
+                    .WithEntrypoint("/bin/sh")
+                    .WithArgs("/ollamasetup.sh");
+
+var ollamacontainerendpoint = ollama.GetEndpoint("ollamaendpoint");
 
 builder.AddProject<Projects.ProductClassification>("productclassification")
         .WithReference(promptevaluationdb)
-        .WaitFor(promptevaluationdb);
+        .WaitFor(promptevaluationdb)
+        .WaitFor(ollama)
+        .AddOllamaEndpointToEnvironmentVariables(ollamacontainerendpoint);
 
 builder.AddProject<Projects.ProductClassificationDatabase_MigrationService>("productclassificationdatabase-migrationservice")
         .WithReference(promptevaluationdb)
